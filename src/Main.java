8d0b4e2b01048a980b7b4102a0c708bbd29e5290
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-27 20:13:27 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,14 @@ public class Main {
         for (int i = 1; i <= 10; i++){
             System.out.println(i);
         }
+
+        // Разделитель
+        System.out.println();
+
+        // Задание 2
+        System.out.println("--- Задание 2 ---");
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
     }
 }
